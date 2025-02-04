dad7186ad7f6e63e16a99373b322b4a6787a6bbb
nikhilbhojak <nikhil.bhojak@kanerika.com>
2025-02-04 14:33:32 +0530

    Button Component

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,17 +1,109 @@
-import React, { ReactElement } from "react" ;
+// import React, { ReactElement } from "react" ;
 
-interface ButtonProps {
-    label : string;
-    type: string;
+// interface ButtonProps {
+//     label : string;
+//     type: string;
     
+// }
+
+// const Button =(props:ButtonProps)=>{
+//     return (
+//         <>
+//            <button className="outline text-2xl p-2">{props.label}</button>
+//         </>
+//     )
+// };
+
+// export default Button;
+
+
+import React, { forwardRef, memo, useMemo } from "react";
+
+type BaseButtonAttributes = React.ComponentPropsWithoutRef<"button">;
+type Ref = HTMLButtonElement;
+
+
+interface ButtonProps extends BaseButtonAttributes {
+    key:string;
+    disabled?: boolean;
+    leftIcon?: React.ReactElement;
+    rightIcon?: React.ReactElement;
+    centerIcon?: React.ReactElement;
+    buttonType?: "primary" | "secondary" | "gradient";
+    className?: string;
 }
 
-const Button =(props:ButtonProps)=>{
-    return (
-        <>
-           <button className="outline text-2xl p-2">{props.label}</button>
-        </>
-    )
+const getButtonStyles = (btntype: "primary" | "secondary" | "gradient", disabled: boolean) => {
+    const baseStyles = `px-4 py-2  rounded-lg font-medium transition duration-200 flex items-center justify-center ${disabled && ' opacity-50'} `;
+
+    const btntypes = {
+        primary: `bg-custom-blue text-white`,
+        secondary: ``,
+        gradient: `bg-gradient-45 hover:bg-gradient-hover text-white`,
+    };
+
+    return `${baseStyles} ${btntypes[btntype]} ${disabled ? "cursor-not-allowed opacity-40" : ""}`;
 };
+
+const Button = memo(forwardRef<Ref, ButtonProps>((props, ref) => {
+    const {
+        type="button", children, buttonType = "primary", disabled=false, leftIcon = undefined, rightIcon = undefined, centerIcon=undefined ,className="", ...rest
+    } = props;
+
+    let lefticonPlacement : string='';
+    let righticonPlacement: string ='';
+    let centericonplacement: string='';
+
+    if(leftIcon){
+        lefticonPlacement='left';
+    }
+    if(rightIcon){
+        righticonPlacement='right';
+    }
+    if(centerIcon){
+        centericonplacement='center'
+    }
+
+    // const { icon, iconPlacement } = useMemo(() => {
+    //     if(rightIcon) {
+    //         return { icon: rightIcon, iconPlacement: 'right' }
+    //     }
+
+    //     if(leftIcon) {
+    //         return { icon: leftIcon, iconPlacement: 'left' }
+    //     }
+
+    //     return { icon: undefined, iconPlacement: undefined };
+    // }, [leftIcon, rightIcon]);
+
+    return (
+        
+            <button
+                className={`${getButtonStyles(buttonType, disabled)} ${className} `}
+                {...rest}
+                type={type ? "submit" : "button"}
+                ref={ref}
+                
+                disabled={disabled}
+            >
+                {/* Left Icon */}
+                { leftIcon && lefticonPlacement === "left" && (
+                    <span className={`mr-2 inline-flex self-center ${children && "space-x-2"}`}>{leftIcon}</span>
+                )}
+                {/* center Icon */}
+                {centerIcon && centericonplacement === "center" && (
+                    <span className={`inline-flex self-center`}>{centerIcon}</span>
+                )}
+
+              {children}
+
+                {/* Right Icon */}
+                { rightIcon&& righticonPlacement === "right" && (
+                    <span className={`ml-2 inline-flex self-center ${children && "space-x-2"}`}>{rightIcon}</span>
+                )}
+            </button>
+        
+    );
+}));
 
 export default Button;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,36 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [ "./src/**/*.tsx"],
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./src/*.{js,jsx}"],
   theme: {
-    extend: {},
+    extend: {     
+      fontFamily: {
+        sans: ['"Space Grotesk"', "Roboto", "ui-sans-serif", "system-ui"],
+      },
+      colors: {
+        "custom-purple": "#8c48ff",
+        "custom-orange": "#ff5f3d",
+        "custom-blue": "#1664df",
+        "custom-hover-purple": "#7a3ede",
+        "custom-hover-orange": "#e65232",
+      },
+      backgroundImage: {
+        "gradient-45": "linear-gradient(45deg, #8c48ff, #ff5f3d)",
+        "gradient-hover": "linear-gradient(45deg, #7a3ede, #e65232)",
+        "gradient-tooltip":
+          "linear-gradient(90deg, rgba(121,29,91,1) 43%, rgba(20,32,140,1) 100%, rgba(28,19,178,1) 100%)",
+        "custom-gradient":
+          "linear-gradient(135deg, rgba(140, 72, 255, 0.2), rgba(22, 100, 223, 0.2))",
+        "custom-gradient-light":
+          "linear-gradient(145deg, rgba(140, 72, 255, 0.1), rgba(22, 100, 223, 0.1), rgba(255, 95, 61, 0.1))",
+        "custom-gradient-ultra-light":
+          "linear-gradient(135deg, rgba(140, 72, 255, 0.05), rgba(22, 100, 223, 0.05))",
+          'custom-sidebar-gradient-ultra-light': 'linear-gradient(145deg, rgba(140, 72, 255, 0.07), rgba(22, 100, 223, 0.07), rgba(255, 95, 61, 0.07))',
+          'spread-gradient': 'linear-gradient(to right, #8c48ff 20%, #b764ff 40%, #ff5f3d 80%)',
+      },
+    },
+  },
+  corePlugins: {
+    preflight: false,  // Disables Tailwind's default base styles
   },
   plugins: [],
-}
-
+};
